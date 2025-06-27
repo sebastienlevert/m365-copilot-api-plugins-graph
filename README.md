5a0ce978d707af9e6be1d196ca2a8092ac33938e
Sébastien Levert <slevert@microsoft.com>
2025-06-27 19:49:35 -0400

    Adding mail and tasks

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # graph-api-plugins
+
 A collection of Microsoft Graph API plugins that can be reused in all your projects.
---
+++ src/lib.ts
@@ -0,0 +1,11 @@
+import { createTypeSpecLibrary } from "@typespec/compiler";
+
+export const $lib = createTypeSpecLibrary({
+  name: "graph-api-plugin",
+  // Define diagnostics for the library. This will provide a typed API to report diagnostic as well as a auto doc generation.
+  diagnostics: {},
+  // Defined state keys for storing metadata in decorator.
+  state: {},
+});
+
+export const { reportDiagnostic, createDiagnostic, stateKeys: StateKeys } = $lib;
